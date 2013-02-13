5575634723bf456e08bd0c542a31a6363bb6114d
Tom Spink <t.spink@sms.ed.ac.uk>
2013-02-13 08:26:40 +0000

    Begin the implementation of the ELF loader.

--- src/loader/elf.c
+++ src/loader/elf.c
@@ -8,8 +8,25 @@
  */
 #include <ksim.h>
 #include <ksim-binary.h>
+#include <elf.h>
+
+static inline int is_elf(Elf32_Ehdr *header)
+{
+	return  header->e_ident[EI_MAG0] == ELFMAG0 &&
+		header->e_ident[EI_MAG1] == ELFMAG1 &&
+		header->e_ident[EI_MAG2] == ELFMAG2 &&
+		header->e_ident[EI_MAG3] == ELFMAG3;
+}
 
 int elf_load(struct ksim_binary *binary)
 {
+	Elf32_Ehdr *header = (Elf32_Ehdr *)binary->base;
+	
+	if (binary->size < sizeof(*header))
+		return -1;
+	
+	if (!is_elf(header))
+		return -1;
+	
 	return -1;
 }
